f8a83a933ddf9358532a11d498626af7ca94de79
Zuburqan <zuburqan@yahoo.com>
2022-11-01 12:29:15 +0000

    Rename fixtures and cleanup

--- test/orchestrator_test.exs
+++ test/orchestrator_test.exs
@@ -2,13 +2,13 @@ defmodule OrchestratorTest do
   use ExUnit.Case
 
   test "reads file 1 and generates answer" do
-    file_path = "test_1.txt"
+    file_path = "fixture_1.txt"
 
     assert Orchestrator.run(file_path) == "Paradise,Cambridge,14.0"
   end
 
   test "reads file 2 and generates answer" do
-    file_path = "test_2.txt"
+    file_path = "fixture_2.txt"
 
     assert Orchestrator.run(file_path) ==
              "Paradise,Cambridge,14.0\nLeyton,Waterloo,11.0\nLeyton,Waterloo,12.0"
